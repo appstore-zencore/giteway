09f318aa2912353215bf11f2e63a22dd9d1c0400
zencore <dobetter@zencore.cn>
2017-12-28 19:31:21 +0800

    no user edit

--- src/giteway/admin.py
+++ src/giteway/admin.py
@@ -6,8 +6,14 @@ class RepoAdmin(admin.ModelAdmin):
     list_display = ["name", "description", "address"]
     search_fields = ["name", "description"]
 
+    fieldsets = [
+        (None, {
+            "fields": ["name", "description"],
+        })
+    ]
+
     def save_model(self, request, obj, form, change):
-        if obj.user is None:
+        if (not hasattr(obj, "user")) or (obj.user is None):
             obj.user = request.user
         super(RepoAdmin, self).save_model(request, obj, form, change)
 
